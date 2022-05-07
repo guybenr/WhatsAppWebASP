74bd33c65f80820ec7b64d7c5ca8c961b21a60b9
adi604 <84729170+adi604@users.noreply.github.com>
2022-05-08 00:15:21 +0300

    start rating page - home page and details

---
+++ MVC/Controllers/ReviewsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        private static List<Review> reviews;
+
+        public ReviewsController()
+        {
+            reviews = new List<Review>();
+                reviews.Add(new Review() { Id = 1, Name = "Adi Aviv", Description = "aaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaa aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa, aaaaaaaaaaaaaaaaa. aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", Grade = 5 });
+                reviews.Add(new Review() { Id = 2, Name = "Guy Ben Razon", Description = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbb , bbbbbbbbbbbbbbbbbbbbbbbbbbb . bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb", Grade = 4 });
+        }
+        public IActionResult Index()
+        {
+            return View(reviews);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Review review = reviews.Find(x => x.Id == id);
+            if (review != null)
+            {
+                return View(review);
+            }
+            return StatusCode(404, "Review Not Found");
+            
+        }
+    }
+}
---
+++ MVC/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace MVC.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public int Grade { get; set; }
+    }
+}
